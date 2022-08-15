264eeebc6dbd50fb161bec190736d148e08959d6
Korppi <3602291+Korppi@users.noreply.github.com>
2022-08-15 22:36:20 +0300

    moved getReceiptsFromJson stuff to getReceipts. added comments

--- lib/src/features/receipts/data/json_receipt_repository.dart
+++ lib/src/features/receipts/data/json_receipt_repository.dart
@@ -1,27 +1,24 @@
 import 'dart:convert';
 
+import 'package:demo_hero_flutter/src/constants/constants.dart';
 import 'package:demo_hero_flutter/src/features/receipts/data/receipts_repository.dart';
 import 'package:demo_hero_flutter/src/features/receipts/model/receipt.dart';
 import 'package:flutter/services.dart';
 
+/// Receipt repository using json file as data source
 class JsonReceiptRepository implements ReceiptRepository {
   @override
   Future<List<Receipt>> getReceipts() async {
-    final results = await _getReceiptsFromJson();
-    return results;
-  }
-
-  Future<List<Receipt>> _getReceiptsFromJson() async {
     final jsonString = await _loadJsonData();
     final jsonData = jsonDecode(jsonString) as List<dynamic>;
     final results =
         jsonData.map((receiptData) => Receipt.fromJson(receiptData)).toList();
-
     return results;
   }
 
+  /// Loads receipt json inside assets folder
   Future<String> _loadJsonData() async {
-    final jsonString = await rootBundle.loadString('assets/receipts.json');
+    final jsonString = await rootBundle.loadString(kReceiptsJsonPath);
     return jsonString;
   }
 }
