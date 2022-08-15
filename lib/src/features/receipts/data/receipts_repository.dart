25c7c5eac81c297ce2678dd72a768da5daccee7c
Korppi <3602291+Korppi@users.noreply.github.com>
2022-08-15 22:35:37 +0300

    added comments

--- lib/src/features/receipts/data/receipts_repository.dart
+++ lib/src/features/receipts/data/receipts_repository.dart
@@ -1,5 +1,7 @@
 import 'package:demo_hero_flutter/src/features/receipts/model/receipt.dart';
 
+/// Interface for Receipt repositories
 abstract class ReceiptRepository {
+  /// Get all receipts from repository
   Future<List<Receipt>> getReceipts();
 }
