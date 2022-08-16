2a41ff620cf6f21f73c6a6c0be983d0d071ae9d0
Korppi <3602291+Korppi@users.noreply.github.com>
2022-08-16 20:04:37 +0300

    added model for receipts summary

---
+++ lib/src/features/receipts/model/receipts_summary.dart
@@ -0,0 +1,16 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'receipts_summary.freezed.dart';
+part 'receipts_summary.g.dart';
+
+@freezed
+class ReceiptsSummary with _$ReceiptsSummary {
+  const factory ReceiptsSummary({
+    @Default(0) int receiptCount,
+    @Default(0) int highestCost,
+    @Default(0) int lowestCost,
+  }) = _ReceiptsSummary;
+
+  factory ReceiptsSummary.fromJson(Map<String, dynamic> json) =>
+      _$ReceiptsSummaryFromJson(json);
+}
