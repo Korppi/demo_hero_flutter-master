ccb6f5a228c0406acdd0e53f03f468f245ba0325
Korppi <3602291+Korppi@users.noreply.github.com>
2022-10-09 00:16:18 +0300

    fixed import path

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:demo_hero_flutter/src/features/receipts/presentation/info_button.dart';
-import 'package:demo_hero_flutter/src/features/receipts/presentation/receipts_list_screen.dart';
+import 'package:demo_hero_flutter/src/features/receipts/presentation/listview_page/info_button.dart';
+import 'package:demo_hero_flutter/src/features/receipts/presentation/listview_page/receipts_list_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
